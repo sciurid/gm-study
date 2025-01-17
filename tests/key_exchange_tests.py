3945cc9270ba5c0bfdce76bba66001e89beb80bb
LanceChen <chenqiang@tsinghua.edu.cn>
2025-01-17 18:35:55 +0800

    Complete sm2 key exchange protocol

--- tests/key_exchange_tests.py
+++ tests/key_exchange_tests.py
@@ -1,19 +1,30 @@
 import unittest
 
 from gmutil import *
-
+import sys
 
 class KeyExchangeTests(unittest.TestCase):
-    def test_key_exchange(self):
-        user_a = SM2KeyExchange(uid='user-a@tsinghua.edu.cn'.encode())
-        user_b = SM2KeyExchange(uid='user-b@tsinghua.edu.cn'.encode())
+    def test_simple_key_exchange(self):
+        user_a = SM2KeyExchange(uid='user-a'.encode())
+        user_b = SM2KeyExchange(uid='user-b'.encode())
 
-        k_byte_len = 16
-        key_b = user_b.receive(user_a.public_key, user_a.point_r, user_a.uid, k_byte_len, False)
-        key_a = user_a.receive(user_b.public_key, user_b.point_r, user_b.uid, k_byte_len, True)
+        key_a = user_a.calculate_key(True, *user_b.send())
+        key_b = user_b.calculate_key(False, *user_a.send())
 
         print("Key A:", key_a.hex())
         print("Key B:", key_b.hex())
-        self.assertEqual(key_a, key_b)
+        sys.stdout.flush()
 
 
+    def test_verified_key_exchange(self):
+        user_a = SM2KeyExchangePartyA(uid='user-a'.encode())
+        user_b = SM2KeyExchangePartyB(uid='user-b'.encode())
+
+        user_b.receive_1(*user_a.send_1())
+        user_a.receive_2(*user_b.send_2())
+        user_b.receive_3(*user_a.send_3())
+
+        print("Key A:", user_a.exchanged_key.hex())
+        print("Key B:", user_b.exchanged_key.hex())
+
+        self.assertEqual(user_a.exchanged_key, user_b.exchanged_key)
