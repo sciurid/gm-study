68817827eece2c2566775761bd6cbda6f2653b89
CHEN Qiang <chenqiang@tsinghua.edu.cn>
2025-02-08 18:33:46 +0800

    优化了SM3的实现，复用了中间存储

--- gmutil/sm3hash.py
+++ gmutil/sm3hash.py
@@ -11,7 +11,7 @@ _SM3_TJ = tuple(0x79cc4519 if 0 <= j < 16 else 0x7a879d8a for j in range(0, 64))
 def rls_32(x: int, n: int):
     """32位循环左移函数（Rotate Left Shift）：x <<< n
     """
-    assert x.bit_length() <= 32
+    # assert x.bit_length() <= 32
     if n >= 32:
         n = n % 32
     return (x << n) & 0xffffffff | (x >> (32 - n))
@@ -32,9 +32,9 @@ def mod_adds_32(*args):
 
 def _sm3_ff_j(x: int, y: int, z: int, j: int):
     """GB/T 32905-2016 4.3 布尔函数FF_j"""
-    assert x.bit_length() <= 32
-    assert y.bit_length() <= 32
-    assert z.bit_length() <= 32
+    # assert x.bit_length() <= 32
+    # assert y.bit_length() <= 32
+    # assert z.bit_length() <= 32
     if 0 <= j < 16:
         ret = x ^ y ^ z
     elif 16 <= j < 64:
@@ -46,9 +46,9 @@ def _sm3_ff_j(x: int, y: int, z: int, j: int):
 
 def _sm3_gg_j(x: int, y: int, z: int, j):
     """GB/T 32905-2016 4.3 布尔函数GG_j"""
-    assert x.bit_length() <= 32
-    assert y.bit_length() <= 32
-    assert z.bit_length() <= 32
+    # assert x.bit_length() <= 32
+    # assert y.bit_length() <= 32
+    # assert z.bit_length() <= 32
     if 0 <= j < 16:
         ret = x ^ y ^ z
     elif 16 <= j < 64:
@@ -60,13 +60,13 @@ def _sm3_gg_j(x: int, y: int, z: int, j):
 
 def _sm3_p0(x: int):
     """GB/T 32905-2016 4.4 置换函数P0"""
-    assert x.bit_length() <= 32
+    # assert x.bit_length() <= 32
     return x ^ rls_32(x, 9) ^ rls_32(x, 17)
 
 
 def _sm3_p1(x: int):
     """GB/T 32905-2016 4.4 置换函数P1"""
-    assert x.bit_length() <= 32
+    # assert x.bit_length() <= 32
     return x ^ rls_32(x, 15) ^ rls_32(x, 23)
 
 
@@ -112,6 +112,7 @@ class SM3Hash:
     def __init__(self):
         self._buffer = bytearray()
         self._cursor = 0
+        self._offset = 0
         self._v = list(_SM3_IV)
 
         self._w = [0] * 68
@@ -124,7 +125,7 @@ class SM3Hash:
         :param block_in: 输入的64字节消息
         :return: 返回的w和w'，分别为68个32bit整数和64个32bit整数，用于CF压缩函数
         """
-        assert len(block_in) == 64
+        # assert len(block_in) == 64
         w = self._w
         j, m, n = 0, 0, 4
         while j < 16:
@@ -147,7 +148,7 @@ class SM3Hash:
         v_i: 迭代压缩输入32 bytes（256 bits）
         b_i: 消息分组输入32 bytes（256 bits）
         """
-        assert len(block_in) == 64
+        # assert len(block_in) == 64
 
         self._expand(block_in)
 
@@ -170,10 +171,16 @@ class SM3Hash:
             self._v[i] = v
 
     def _process_block(self):
+        buffer_view = memoryview(self._buffer)
         while (next_cursor := self._cursor + 64) <= len(self._buffer):
-            block_in = memoryview(self._buffer)[self._cursor:next_cursor]
+            block_in = buffer_view[self._cursor:next_cursor]
             self._cf(block_in)
             self._cursor = next_cursor
+        buffer_view.release()
+
+        self._buffer = self._buffer[self._cursor:]
+        self._offset += self._cursor
+        self._cursor = 0
 
     def update(self, message: bytes):
         self._buffer.extend(message)
@@ -182,7 +189,7 @@ class SM3Hash:
             self._cursor = next_cursor
 
     def digest(self) -> bytes:
-        mbl = len(self._buffer)  # 消息的最终长度（字节数）
+        mbl = len(self._buffer) + self._offset  # 消息的最终长度（字节数）
         l = mbl * 8  # 消息的最终长度（比特数）
         k = (mbl + 9) % 64  # 消息加0x80填充+8字节长度结尾填充以后，还需要填充0x00的字节数
         self._buffer.append(0x80)  # 消息填充
@@ -196,7 +203,4 @@ class SM3Hash:
         for n in self._v:
             result.extend(n.to_bytes(4, byteorder='big', signed=False))
         return bytes(result)
-
-
-
 
